ed3f4e4609ceae3b94aef13c43310ff5022e6bcd
Jadhiel Vélez <jadhielv@gmail.com>
2020-02-13 01:50:44 -0400

    update employee component

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { EmployeeService } from '../services/employee.service';
+import { Employee } from '../models/employee';
 
 @Component({
   selector: 'app-employee',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent implements OnInit {
+  employee$: Observable<Employee>;
+  employeeId: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private employeeService: EmployeeService, private avRoute: ActivatedRoute) {
+    const idParam = 'id';
+    if (this.avRoute.snapshot.params[idParam]) {
+      this.employeeId = this.avRoute.snapshot.params[idParam];
+    }
   }
 
+  ngOnInit() {
+    this.loadEmployee();
+  }
+
+  loadEmployee() {
+    this.employee$ = this.employeeService.getEmployee(this.employeeId);
+  }
 }
