22d1efb2a68ffd94499bc1f8168ba83d1138491a
Jadhiel Vélez <jadhielv@gmail.com>
2020-02-12 23:56:43 -0400

    setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeAddEditComponent } from './employee-add-edit/employee-add-edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EmployeesComponent, pathMatch: 'full' },
+  { path: 'employee/:id', component: EmployeeComponent },
+  { path: 'add', component: EmployeeAddEditComponent },
+  { path: 'employee/edit/:id', component: EmployeeAddEditComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
