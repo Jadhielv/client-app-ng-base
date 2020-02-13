d9cc965508cf6862ca5b8e96c1549bb30440eca1
Jadhiel Vélez <jadhielv@gmail.com>
2020-02-13 01:32:36 -0400

    update employees component

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EmployeeService } from '../services/employee.service';
+import { Employee } from '../models/employee';
 
 @Component({
   selector: 'app-employees',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees.component.scss']
 })
 export class EmployeesComponent implements OnInit {
+  employees$: Observable<Employee[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private employeeService: EmployeeService) {
   }
 
+  ngOnInit() {
+    this.loadEmployees();
+  }
+
+  loadEmployees() {
+    this.employees$ = this.employeeService.getEmployees();
+  }
+
+  delete(employeeId) {
+    const ans = confirm('Do you want to delete employee with id: ' + employeeId);
+    if (ans) {
+      this.employeeService.deleteBlogPost(employeeId).subscribe((data) => {
+        this.loadEmployees();
+      });
+    }
+  }
 }
