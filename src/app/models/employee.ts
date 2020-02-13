1e258394622c6848fd32bf245f16f3ce93d7c6b9
Jadhiel Vélez <jadhielv@gmail.com>
2020-02-13 00:23:47 -0400

    add employee model and service methods

---
+++ src/app/models/employee.ts
@@ -0,0 +1,14 @@
+export class Employee {
+  employeeId?: number;
+  firstName: string;
+  lastName: string;
+  country: string;
+  email: string;
+  createdAt: Date;
+  state: string;
+  city: string;
+  street: string;
+  zipCode: string;
+  userName: string;
+  ipAddress: string;
+}
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,79 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Employee } from '../models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  myAppUrl: string;
+  myApiUrl: string;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8'
+    })
+  };
+
+  constructor(private http: HttpClient) {
+    this.myAppUrl = environment.appUrl;
+    this.myApiUrl = 'api/Employees/';
+  }
+
+  getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.myAppUrl + this.myApiUrl)
+      .pipe(
+        retry(1),
+        catchError(this.errorHandler)
+      );
+  }
+
+  getEmployee(employeeId: number): Observable<Employee> {
+    return this.http.get<Employee>(this.myAppUrl + this.myApiUrl + employeeId)
+      .pipe(
+        retry(1),
+        catchError(this.errorHandler)
+      );
+  }
+
+  saveEmployee(employee: any): Observable<Employee> {
+    return this.http.post<Employee>(this.myAppUrl + this.myApiUrl, JSON.stringify(employee), this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.errorHandler)
+      );
+  }
+
+  updateEmployee(employeeId: number, employee: any): Observable<Employee> {
+    return this.http.put<Employee>(this.myAppUrl + this.myApiUrl + employeeId, JSON.stringify(employee), this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.errorHandler)
+      );
+  }
+
+  deleteBlogPost(employeeId: number): Observable<Employee> {
+    return this.http.delete<Employee>(this.myAppUrl + this.myApiUrl + employeeId)
+      .pipe(
+        retry(1),
+        catchError(this.errorHandler)
+      );
+  }
+
+  errorHandler(error: { error: { message: string; }; status: any; message: any; }) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 }
